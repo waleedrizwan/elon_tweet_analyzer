69e4ee31f66bd9c408bc76b6eb25473641e1cee4
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2020-10-04 12:40:32 -0400

    switched to tweepy

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -10,36 +10,46 @@ Created on Sun Sep 27 19:31:59 2020
 #access_token_key ='AAAAAAAAAAAAAAAAAAAAAPZOIAEAAAAA%2FZ17URJXP8A70lafAvZseMwsShE%3DJTNVn6ltA37TaghBmtKyPmvhN9qIJwDFGrCeHgP0BLuRWoRuhf')
 
 
+
+
 # requires authentation from Twittter Developer Community 
-import twitter
-import sys
-
-api = twitter.Api(consumer_key='MdLc2PWvaUh4ZCQn0DJESoe4n',
-                  consumer_secret='M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL',
-                  access_token_key='1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC',
-                  access_token_secret='RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo')
-
-user = 'elonmusk'
-statuses = api.GetUserTimeline(screen_name=user)
 
 
-# prints out recent tweets
-tweets = [] 
-for status in statuses:
-    #print(status.text)
-    tweets.append(status.text)
+import tweepy 
+import pandas as pd 
+
+auth = tweepy.OAuthHandler('MdLc2PWvaUh4ZCQn0DJESoe4n', 'M7PqFUKvpV2MtnvvLRpo0mvg5ZDQgaa5JlavUxy5YgFlDONEeL')
+auth.set_access_token('1309966965663035392-4YY7ERbSPWTFyPoAHwCQr0K7c0FtYC', 'RPBhXHjoumebhiURAQ7D5HZtq2EaBFwHsoZKQxn7bBiXo')
+
+api = tweepy.API(auth)
+
+tweetList = api.user_timeline(screen_name='elonmusk', tweet_mode="extended", count = 200)
+actualTextList = []
+
+
+for x in tweetList:
+    print(x.full_text.replace("@", "").replace("RT", "")+ '\n')
+    actualTextList.append(x.full_text.replace("@", "").replace("RT", ""))
  
     
+'''   
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+df
+   col1  col2
+0     1     3
+1     2     4   
 '''
-# returns 100 most recent tweets when Twitter is searched on the application 
-results = api.GetSearch(
-    raw_query="q=Tesla%20&result_type=recent&since=2014-07-19&count=100000")
 
-searchForTweets = []
-for r in results:
-    searchForTweets.append(r.text)
-'''
+  
+col1 = {'tweet': actualTextList}
+df = pd.DataFrame(data=col1)
 
+## Next step remove non alphanumeric characters
 
+## manually add labels to each tweet 
+## train ML algo using the logic from the Udemy course 
     
+
+
     
